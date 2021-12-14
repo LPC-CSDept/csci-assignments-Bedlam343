939dc68ae52d5a5d709f8cc5b3ebb6db81822e29
Bedlam343 <jagik22@gmail.com>
2021-12-13 19:01:29 -0800

    Update 3

--- Final/final1.asm
+++ Final/final1.asm
@@ -6,16 +6,16 @@
     .globl main
 
 main:
-    li  $t0, 1              # counter for loop
-    li  $t1, 3              # loop counter limit
-    lui $t2, 0xFFFF         # load address of Receiver Control
+        li  $t0, 1              # counter for loop
+        li  $t1, 3              # loop counter limit
+        lui $t2, 0xFFFF         # load address of Receiver Control
 
 rd_wait:
 
-    blt $t0, $t1, rd_wait   # if ($t0 < $t1) loop back
-    nop                     # delay slot for the branch
+        blt $t0, $t1, rd_wait   # if ($t0 < $t1) loop back
+        nop                     # delay slot for the branch
 
-    li  $v0, 10             # service code to end program
-    syscall                 # End program
+        li  $v0, 10             # service code to end program
+        syscall                 # End program
 
 ## End of program
