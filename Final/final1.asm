d607c142986fc2f2cc26f770212891ed46369e6f
Bedlam343 <jagik22@gmail.com>
2021-12-13 19:14:44 -0800

    Update 7

--- Final/final1.asm
+++ Final/final1.asm
@@ -9,14 +9,15 @@ main:
         li      $t0, 1                  # counter for loop
         li      $t1, 3                  # loop counter limit
         lui     $t2, 0xFFFF             # load address of Receiver Control
+        li      $t3, 100                # Multiplier
 
 rd_wait:
-        lw      $t3, 0( $t2 )           # load bits of Receiver Control Register
-        andi    $t3, $t3, 0x0001        # clear all bits except the Lowest Order Bit
-        beq     $t3, $zero, rd_wait     # Input device is not ready, so loop back
+        lw      $t4, 0( $t2 )           # load bits of Receiver Control Register
+        andi    $t4, $t4, 0x0001        # clear all bits except the Lowest Order Bit
+        beq     $t4, $zero, rd_wait     # Input device is not ready, so loop back
         nop                             # delay slot for the branch
 
-        lw      $t3, 4( $t2 )           # Input device is ready, so read value from Receiver Data Register
+        lw      $s0, 4( $t2 )           # Input device is ready, so read value from Receiver Data Register
 
         beq     $t0, $t1, end           # The loop counter is reached, so break out of the loop
         nop                             # delay slot for the branch
