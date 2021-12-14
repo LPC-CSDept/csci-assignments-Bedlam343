a2f594ba59d81bc14364486250b744cdd583fd22
Bedlam343 <jagik22@gmail.com>
2021-12-13 19:05:59 -0800

    Update 5

--- Final/final1.asm
+++ Final/final1.asm
@@ -6,16 +6,22 @@
         .globl main
 
 main:
-        li      $t0, 1              # counter for loop
-        li      $t1, 3              # loop counter limit
-        lui     $t2, 0xFFFF         # load address of Receiver Control
+        li      $t0, 1                  # counter for loop
+        li      $t1, 3                  # loop counter limit
+        lui     $t2, 0xFFFF             # load address of Receiver Control
 
 rd_wait:
-        lw      $t3, 0( $t2 )       # load bits of Receiver Control Register
-        blt     $t0, $t1, rd_wait   # if ($t0 < $t1) loop back
-        nop                         # delay slot for the branch
+        lw      $t3, 0( $t2 )           # load bits of Receiver Control Register
+        andi    $t3, $t3, 0x0001        # clear all bits except the Lowest Order Bit
+        beq     $t3, $zero, rd_wait     # Input device is not ready, so loop back
+        nop                             # delay slot for the branch
 
-        li      $v0, 10             # service code to end program
-        syscall                     # End program
+        
+
+        blt     $t0, $t1, rd_wait       # if ($t0 < $t1) loop back
+        nop                             # delay slot for the branch
+
+        li      $v0, 10                 # service code to end program
+        syscall                         # End program
 
 ## End of program
