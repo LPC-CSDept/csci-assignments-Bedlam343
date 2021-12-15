ca804510627f2cb5fca18ec8f0b735841ff228b8
Bedlam343 <jagik22@gmail.com>
2021-12-15 13:30:04 -0800

    Update 1

---
+++ Final/final2.asm
@@ -0,0 +1,13 @@
+# Jagjit Singh
+# Question 2
+# 12/15/2021
+
+        .text
+        .globl main
+
+main:
+
+        li      $v0, 10                 # service code to end program
+        syscall                         # exit if the program gets here
+
+## End of program
